d02e8d7c61f1b00d76ab5fedee7b0e1b173ac903
iremchabanne <irem_kcakir@outlook.com>
2024-07-12 14:12:17 +0200

    Back-end for videos

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -14,6 +14,10 @@ const userRouter = require("./users/router");
 
 router.use("/users", userRouter);
 
+const videosRouter = require("./videos/router");
+
+router.use("/videos", videosRouter);
+
 /* ************************************************************************* */
 
 module.exports = router;
