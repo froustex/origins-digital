a7603d480132de504440390daf8f3f18ebbc71c8
iremchabanne <irem_kcakir@outlook.com>
2024-07-17 12:29:29 +0200

    wip

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,24 +1,31 @@
 import { Carousel } from "react-responsive-carousel";
 import ReactPlayer from "react-player";
+import { useLoaderData } from "react-router-dom";
 import HeroCarousel from "../components/HeroCarousel";
-import videos from "../components/videos";
 
 export default function Home() {
+  const videos = useLoaderData();
+
   return (
     <main className="bg-zinc-900">
       <div>
-        <HeroCarousel />
+        <HeroCarousel videos={videos} />
       </div>
-      <div className="bg-zinc-900">
-        <h2 className="text-cyan-400">Recently Added</h2>
-        <div className="gap-2.5">
-          <Carousel showStatus={false} centerMode centerSlidePercentage={25}>
+      <container className="bg-zinc-900">
+        <h2 className="text-cyan-400">Category A</h2>
+        <div>
+          <Carousel
+            className="flex w-40"
+            showStatus={false}
+            centerMode
+            centerSlidePercentage={25}
+          >
             {videos.map((video) => (
               <ReactPlayer key={video.id} url={video.source} controls />
             ))}
           </Carousel>
         </div>
-      </div>
+      </container>
     </main>
   );
 }
